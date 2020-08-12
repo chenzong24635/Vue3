c0fa7ccb01aef3654f466e860f52200bea4dc62c
chenzong24635 <chenzong24635@163.com>
2020-08-12 17:17:12 +0800

    打包路径

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
-
-
+console.log(process.env);
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ?  './': '/',
   productionSourceMap: false,
   pluginOptions: {
     'style-resources-loader': {
